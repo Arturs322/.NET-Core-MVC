69d310c1c3c3aebc915777dc8f4b6651c138593b
Arturs322 <krustsarturs@gmail.com>
2023-12-08 12:16:59 +0200

    use ValidateNever

--- Udemy.Models/Product.cs
+++ Udemy.Models/Product.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace Udemy.Models
 {
@@ -43,7 +44,9 @@ namespace Udemy.Models
 
         public int CategoryId { get; set; }
         [ForeignKey("CategoryId")]
+        [ValidateNever]
         public Category Category { get; set; }
+        [ValidateNever]
         public string imageUrl { get; set; }
 
     }
