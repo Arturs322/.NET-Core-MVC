e2b3bc6b6a958e3138c5e75ec556147e2b677964
Arturs322 <krustsarturs@gmail.com>
2023-12-11 17:22:37 +0200

    add applicationuser and shoppingcart to repository/unitofwork

---
+++ Udemy.DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Udemy.DataAccess.Data;
+using Udemy.DataAccess.Repository.IRepository;
+using Udemy.Models;
+
+namespace Udemy.DataAccess.Repository
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+    {
+        private ApplicationDbContext _db;
+
+        public ApplicationUserRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}
