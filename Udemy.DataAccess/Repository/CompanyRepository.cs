be344eb5244cbeac359b9c7d60e3c480ef200a66
Arturs322 <krustsarturs@gmail.com>
2023-12-11 16:00:55 +0200

    Assignment - 3: Add Company to Repository and UnitOfWork

---
+++ Udemy.DataAccess/Repository/CompanyRepository.cs
@@ -0,0 +1,21 @@
+﻿using Udemy.DataAccess.Data;
+using Udemy.DataAccess.Repository.IRepository;
+using Udemy.Models;
+
+namespace Udemy.DataAccess.Repository
+{
+    public class CompanyRepository : Repository<Company>, ICompanyRepository
+    {
+        private ApplicationDbContext _db;
+
+        public CompanyRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Company obj)
+        {
+            _db.Companies.Update(obj);
+        }
+    }
+}
