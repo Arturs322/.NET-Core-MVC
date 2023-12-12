f8e50a699cf3e62ae1655a6429e68d04a727e1ce
Arturs322 <krustsarturs@gmail.com>
2023-12-12 11:02:06 +0200

    use filter on GetAll()

--- Udemy.DataAccess/Repository/Repository.cs
+++ Udemy.DataAccess/Repository/Repository.cs
@@ -39,10 +39,14 @@ namespace Udemy.DataAccess.Repository
             return query.FirstOrDefault();
         }
 
-        public IEnumerable<T> GetAll(string? includeProperties = null)
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter, string? includeProperties = null)
         {
             IQueryable<T> query = DbSet;
-            if(!string.IsNullOrEmpty(includeProperties))
+            if(filter != null)
+            {
+                query = query.Where(filter);
+            }
+            if (!string.IsNullOrEmpty(includeProperties))
             {
                 foreach(var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
                 {
