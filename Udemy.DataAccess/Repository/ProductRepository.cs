c0cc06dfe232922eedf6138c164e68f77d10db26
Arturs322 <krustsarturs@gmail.com>
2023-12-15 12:41:51 +0200

    insert multiple images, display them, bs carousel on details

--- Udemy.DataAccess/Repository/ProductRepository.cs
+++ Udemy.DataAccess/Repository/ProductRepository.cs
@@ -25,6 +25,7 @@ namespace Udemy.DataAccess.Repository
                 objFromDb.Description = obj.Description;
                 objFromDb.CategoryId = obj.CategoryId;
                 objFromDb.Author = obj.Author;
+                objFromDb.ProductImages = obj.ProductImages;
                 //if(obj.imageUrl != null)
                 //    {
                 //        objFromDb.imageUrl = obj.imageUrl;
